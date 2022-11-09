634118931fd77ef154089cc31536ad799408bbe4
Jim Zajkowski <jamesez@umich.edu>
2022-11-08 21:10:35 -0500

    change test to example

--- iconic_test.go
+++ iconic_test.go
@@ -1,10 +1,8 @@
 package main
 
-import "testing"
+import "fmt"
 
-func TestHashing(t *testing.T) {
-	want := "2f5bbd5c5147380eecd94daa0cf0681c30ca43803f10868a2b9a2aedd26b0f21"
-	if got := HashFile("test.png"); got != want {
-		t.Errorf("HashFile('test.png') = %q, want %q", got, want)
-	}
+func ExampleHashFile() {
+	fmt.Println(HashFile("test.png"))
+	// Output: 2f5bbd5c5147380eecd94daa0cf0681c30ca43803f10868a2b9a2aedd26b0f21
 }
